7585e19a7ce6943e09650ea7245873d01fc775ff
hicoldcat <hicoldcat@foxmail.com>
2017-04-03 11:31:06 +0800

    add myexpress style and construct

--- app/pages/personal/personal.js
+++ app/pages/personal/personal.js
@@ -1,8 +1,16 @@
+var app = getApp()
 Page({
   data: {
+    motto: '欢迎使用快递喵！',
+    userInfo: {}
   },
+
   onLoad: function () {
-    console.log('onLoad')
-   
+    var that = this
+    app.getUserInfo(function (userInfo) {
+      that.setData({
+        userInfo: userInfo
+      })
+    })
   }
 })
